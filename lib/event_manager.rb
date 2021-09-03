d65fb86e045099915451adabe6f6b2f168948466
jordyf15 <jordyferdian88@gmail.com>
2021-09-03 18:55:32 +0700

    created clean_phone_number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,25 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
+end
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,'0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+  formatted_phone_number = phone_number.to_s.split('').select {|digit| digit.numeric?}
+  if formatted_phone_number.size == 10
+    formatted_phone_number.join('')
+  elsif formatted_phone_number.size == 11 && formatted_phone_number[0] == '1'
+    formatted_phone_number[1..10].join('')
+  else
+    "Invalid Phone number"
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -43,17 +60,12 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
+# contents.each do |row|
+#   id = row[0]
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislators_by_zipcode(zipcode)
+#   phone_number = clean_phone_number(row[:homephone])
+# end
 
-  zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
-
-  # puts "#{name} #{zipcode} #{legislators}"
-end
